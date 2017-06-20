63bbb48e66b2382883e302967ea53b72f855571d
Vignesh-sopho <chiggy97.vn@gmail.com>
2017-06-20 18:53:46 +0530

    implemented step 1 of algorithm

--- anyDBC.cpp
+++ anyDBC.cpp
@@ -14,23 +14,27 @@ using namespace std;
 
 map<int, set<int> > neighbourMap; // this will map a point to a set(the set will be the epsilon neighbours of the point)
 double** dataSet; // this is a list of all points
-vector<long long int> rangeQueryPerformed;
+set<int> rangeQueryPerformed;
 map<int, string> coreList;
+map<int, string> borderList;
+set<int> noiseList;
+map<int, set<int> > coreForPointMap;
+set<int> neiNoise;
 int alpha = 100;
 int minDist = 2;
 int minPts = 5;
 
-double** readData(char* file_name,long long int* num_records, int* dimension);
-vector<long long int> pick(long long int N, int k);
-std::unordered_set<int> pickSet(long long int N, int k, std::mt19937& gen);
+double** readData(char* file_name,int* num_records, int* dimension);
+vector<int> pick(int N, int k);
+std::unordered_set<int> pickSet(int N, int k, std::mt19937& gen);
 void anyDBC(int num_records, int dimension);
-vector<long long int> getRandomPoints(vector<long long int> untouchedList);
+vector<int> getRandomPoints(vector<int> untouchedList);
 double distance(double* a,double* b, int dimension);
-set<long long int> performRangeQuery(int point, long long int num_records,int dimension);
+set<int> performRangeQuery(int point, int num_records,int dimension);
 
 //typedef std::numeric_limits< double > db1;
 
-double** readData(char* file_name,long long int* num_records, int* dimension)
+double** readData(char* file_name,int* num_records, int* dimension)
 {
 	ifstream fp;
 	fp.open(file_name);
@@ -38,7 +42,7 @@ double** readData(char* file_name,long long int* num_records, int* dimension)
 	fp >> *num_records;
 	fp >> *dimension;
 	double** dataSet = (double**)malloc((*num_records)*sizeof(double*));
-	long long int record_num,field_num;
+	int record_num,field_num;
 	for(record_num = 0; record_num < *num_records; record_num++)
 	{
 		dataSet[record_num] = (double*)malloc((*dimension)*sizeof(double));
@@ -56,7 +60,7 @@ double** readData(char* file_name,long long int* num_records, int* dimension)
 }
 
 
-vector<long long int> pick(long long int N, int k) {
+vector<int> pick(int N, int k) {
     std::random_device rd;
     std::mt19937 gen(rd());
 
@@ -66,15 +70,15 @@ vector<long long int> pick(long long int N, int k) {
     // it's in a [unknown] deterministic order.
     // so we have to shuffle it:
 
-    vector<long long int> result(elems.begin(), elems.end());
+    vector<int> result(elems.begin(), elems.end());
     std::shuffle(result.begin(), result.end(), gen);
     return result;
 }
 
-std::unordered_set<int> pickSet(long long int N, int k, std::mt19937& gen)
+std::unordered_set<int> pickSet(int N, int k, std::mt19937& gen)
 {
     std::unordered_set<int> elems;
-    for (long long int r = N - k; r < N; ++r) {
+    for (int r = N - k; r < N; ++r) {
         int v = std::uniform_int_distribution<>(1, r)(gen);
 
         // there are two cases.
@@ -91,9 +95,9 @@ std::unordered_set<int> pickSet(long long int N, int k, std::mt19937& gen)
 }
 
 
-vector<long long int> getRandomPoints(vector<long long int> untouchedList)
+vector<int> getRandomPoints(vector<int> untouchedList)
 {
-	vector<long long int> randomPoints;
+	vector<int> randomPoints;
 	int i;
 	if(untouchedList.size() <= alpha)
 	{
@@ -105,7 +109,7 @@ vector<long long int> getRandomPoints(vector<long long int> untouchedList)
 	}
 	else
 	{
-		vector<long long int> ranList = pick(untouchedList.size(),alpha);
+		vector<int> ranList = pick(untouchedList.size(),alpha);
 		for(i = 0; i < ranList.size(); i++)
 		{
 			randomPoints.push_back(untouchedList.at(ranList.at(i)));
@@ -126,13 +130,13 @@ double distance(double* a,double* b, int dimension)
 	return sqrt(distance);
 }
 
-set<long long int> performRangeQuery(int point, long long int num_records,int dimension)
+set<int> performRangeQuery(int point, int num_records,int dimension)
 {
 	double* point_a_coordinates = dataSet[point];
 	double* point_b_coordinates;
-	rangeQueryPerformed.push_back(point);
-	set<long long int> neighbourPoints;
-	long long int i;
+	rangeQueryPerformed.insert(point);
+	set<int> neighbourPoints;
+	int i;
 	for(i = 0; i < num_records; i++)
 	{
 		point_b_coordinates = dataSet[i];
@@ -151,37 +155,140 @@ set<long long int> performRangeQuery(int point, long long int num_records,int di
 	return neighbourPoints;
 }
 
-vector<long long int> assignStateNei(long long int point, vector<long long int> listOfNeighbors)
+vector<int> assignStateNei(int point, set<int> listOfNeighbors)
 {
-	vector<long long int> touchList;
-	neighbourMap[point] = listOfNeighbors;
+	vector<int> touchList;
+	map<int, string>::iterator itr;			//iterator for borderList
+	map<int, string>::iterator itr_core;	//iterator for coreList
+	vector<int>::iterator itr_vec;			//iterator for touchList		
+	map<int, set<int> >::iterator it_cfpm; 	//iterator for coreForPointMap
+	map<int, set<int> >::iterator it_nm;	//iterator for neighbourMap
+	set<int>::iterator it_rqp;			//iterator for rangeQueryPerformed
+	set<int>::iterator it_nn;			//iterator for neiNoise
+	set<int>::iterator itr_nei;				//iterator for listOfNeighbours
+	neighbourMap.insert(pair <int, set<int> >(point, listOfNeighbors));
+	int i;
 	if(listOfNeighbors.size() >= minPts)
 	{
-
+		coreList.insert(pair <int, string>(point,"PROCESSED"));
+		itr = borderList.find(point);
+		if(itr != borderList.end())
+		{
+			borderList.erase(itr);
+		}
+		
+		for(itr_nei = listOfNeighbors.begin(); itr_nei != listOfNeighbors.end(); itr++)
+		{
+			int nei = *itr_nei;
+			itr_vec = find(touchList.begin(),touchList.end(),nei);
+			if(itr_vec == touchList.end())
+			{
+				touchList.push_back(nei);
+			}
+			it_cfpm = coreForPointMap.find(nei);
+			if(it_cfpm == coreForPointMap.end())
+			{
+				set<int> s;
+				coreForPointMap.insert(pair<int,set<int> >(nei,s));
+			}
+			coreForPointMap[nei].insert(point);
+			it_rqp = find(rangeQueryPerformed.begin(),rangeQueryPerformed.end(),nei);
+			if(it_rqp == rangeQueryPerformed.end())
+			{
+				it_nm = neighbourMap.find(nei);
+				if(it_nm == neighbourMap.end())
+				{
+					set<int> s;
+					neighbourMap.insert(pair<int,set<int> >(nei,s));
+				}
+				neighbourMap[nei].insert(point);
+				if(neighbourMap[nei].size() < minPts)
+				{
+					itr = borderList.find(nei);
+					if(itr == borderList.end())
+					{
+						it_rqp = find(rangeQueryPerformed.begin(),rangeQueryPerformed.end(),nei);
+						if(it_rqp != rangeQueryPerformed.end())
+						{
+							borderList.insert(pair<int,string>(nei,"PROCESSED"));
+						}
+						else
+						{
+							borderList.insert(pair<int,string>(nei,"UNPROCESSED"));
+						}
+					}
+				}
+				else
+				{
+					itr_core = coreList.find(nei);
+					if(itr_core == coreList.end())
+					{
+						coreList.insert(pair<int,string>(nei,"UNPROCESSED"));
+						itr = borderList.find(nei);
+						if(itr != borderList.end())
+						{
+							borderList.erase(itr);
+						}
+					}
+				}
+			}
+		}
 	}
+	else
+	{
+		noiseList.insert(point);
+		it_nn = find(neiNoise.begin(),neiNoise.end(),point);
+		if(it_nn != neiNoise.end())
+		{
+			neiNoise.erase(point);
+		}
+		for(itr_nei = listOfNeighbors.begin(); itr_nei != listOfNeighbors.end(); itr++)
+		{
+			int nei = *itr_nei;
+			itr_vec = find(touchList.begin(),touchList.end(),nei);
+			if(itr_vec == touchList.end())
+			{
+				touchList.push_back(nei);
+			}
+			itr_core = coreList.find(nei);
+			itr = borderList.find(nei);
+			if(itr_core == coreList.end() && itr == borderList.end())
+			{
+				neiNoise.insert(nei);
+			}
+			it_nm = neighbourMap.find(nei);
+			if(it_nm == neighbourMap.end())
+			{
+				set<int> s;
+				neighbourMap.insert(pair<int,set<int> >(nei,s));
+			}
+			neighbourMap[nei].insert(point);
+		}
+	}
+	return touchList;
 }
 
 void anyDBC(int num_records, int dimension)
 {	//STARTING STEP:1
-	vector<long long int> untouchedList;
-	vector<long long int>::iterator itr;
-	long long int i;
+	vector<int> untouchedList;
+	vector<int>::iterator itr;
+	int i;
 	for(i = 0; i < num_records; i++)
 	{
 		untouchedList.push_back(i);		
 	}
 	while(untouchedList.size() > 0)
 	{
-		vector<long long int> randomPoints = getRandomPoints(untouchedList);
+		vector<int> randomPoints = getRandomPoints(untouchedList);
 		for(i = 0; i < randomPoints.size(); i++)
 		{
-			long long int point = randomPoints.at(i);
+			int point = randomPoints.at(i);
 			itr = find(untouchedList.begin(),untouchedList.end(),point);
 			if(itr != untouchedList.end()) // test this function
 			{
 				untouchedList.erase(itr);
-				set<long long int> neighbours_of_point = performRangeQuery(point,num_records,dimension);
-				touch = assignStateNei(point,neighbours_of_point);
+				set<int> neighbours_of_point = performRangeQuery(point,num_records,dimension);
+				vector<int> touch = assignStateNei(point,neighbours_of_point);
 
 			}
 		}
@@ -193,7 +300,7 @@ void anyDBC(int num_records, int dimension)
 int main(int argc, char* argv[])
 {
 	char* file_name = argv[1];
-	long long int num_records = 0; // number of records in file
+	int num_records = 0; // number of records in file
 	int dimension = 0; // number of fields in each record;
 	
 
