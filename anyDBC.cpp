c12d0d393a42266cb16a5de376edc61735d19ba1
Vignesh-sopho <chiggy97.vn@gmail.com>
2017-06-23 23:21:20 +0530

    implemented step 2 and 3

--- anyDBC.cpp
+++ anyDBC.cpp
@@ -20,6 +20,14 @@ map<int, string> borderList;
 set<int> noiseList;
 map<int, set<int> > coreForPointMap;
 set<int> neiNoise;
+map<int, set<int> > clusters;
+map<int, set<int> > edgeNo;
+map<int, set<int> > edgeYes;
+map<int, set<int> > edgeWeak;
+map<int, set<int> > edgeUnknown;
+map<int, int> visitedNode;
+
+
 int alpha = 100;
 int minDist = 2;
 int minPts = 5;
@@ -31,7 +39,11 @@ void anyDBC(int num_records, int dimension);
 vector<int> getRandomPoints(vector<int> untouchedList);
 double distance(double* a,double* b, int dimension);
 set<int> performRangeQuery(int point, int num_records,int dimension);
-
+vector<int> assignStateNei(int point, set<int> listOfNeighbors);
+void createPCIR(int point);
+int ddcBetPCIR(int core1,int core2,int dimension);
+void connComp();
+void DFS(int u, int rep);
 //typedef std::numeric_limits< double > db1;
 
 double** readData(char* file_name,int* num_records, int* dimension)
@@ -177,7 +189,7 @@ vector<int> assignStateNei(int point, set<int> listOfNeighbors)
 			borderList.erase(itr);
 		}
 		
-		for(itr_nei = listOfNeighbors.begin(); itr_nei != listOfNeighbors.end(); itr++)
+		for(itr_nei = listOfNeighbors.begin(); itr_nei != listOfNeighbors.end(); itr_nei++)
 		{
 			int nei = *itr_nei;
 			itr_vec = find(touchList.begin(),touchList.end(),nei);
@@ -242,7 +254,7 @@ vector<int> assignStateNei(int point, set<int> listOfNeighbors)
 		{
 			neiNoise.erase(point);
 		}
-		for(itr_nei = listOfNeighbors.begin(); itr_nei != listOfNeighbors.end(); itr++)
+		for(itr_nei = listOfNeighbors.begin(); itr_nei != listOfNeighbors.end(); itr_nei++)
 		{
 			int nei = *itr_nei;
 			itr_vec = find(touchList.begin(),touchList.end(),nei);
@@ -268,10 +280,173 @@ vector<int> assignStateNei(int point, set<int> listOfNeighbors)
 	return touchList;
 }
 
+
+void createPCIR(int point)
+{
+	set<int> s;
+	clusters.insert(pair<int,set<int> >(point,s));
+	clusters[point].insert(point);
+}
+
+int ddcBetPCIR(int core1,int core2,int dimension)
+{
+	double dist = distance(dataSet[core1],dataSet[core2],dimension);
+	if(dist > 3*minDist)
+	{
+		return 1;
+	}
+	set<int> pointPCIR = neighbourMap[core1];
+	pointPCIR.insert(core1);
+	set<int> neiPointPCIR = neighbourMap[core2];
+	neiPointPCIR.insert(core2);
+	set<int> intersectionPoints;
+	set_intersection(pointPCIR.begin(),pointPCIR.end(),neiPointPCIR.begin(),neiPointPCIR.end(),std::inserter(intersectionPoints,intersectionPoints.begin()));
+	set<int> coreListKeys;
+	map<int, string>::iterator map_itr;
+	for(map_itr = coreList.begin(); map_itr != coreList.end(); map_itr++)
+	{
+		coreListKeys.insert(map_itr->first);
+	}
+	set<int> coreIntersectionPoints;
+	set_intersection(intersectionPoints.begin(),intersectionPoints.end(),coreListKeys.begin(),coreListKeys.end(),std::inserter(coreIntersectionPoints,coreIntersectionPoints.begin()));
+	int iCore = coreIntersectionPoints.size();
+	if((iCore > 0) || ((dist > 1.732*minDist)&&(intersectionPoints.size() >= minPts)))
+	{
+		return 0;
+	}
+	else if(intersectionPoints.size() > 0)
+	{
+		return 2;
+	}
+	else
+	{
+		return 3;
+	}
+}
+
+void connComp()
+{
+	set<int> repComp;
+	visitedNode.clear();
+	set<int> edgeYesKeys;
+	map<int,set<int> >::iterator edgeYesItr;
+	map<int,int>::iterator visited_itr;
+	for(edgeYesItr = edgeYes.begin(); edgeYesItr != edgeYes.end(); edgeYesItr++)
+	{
+		edgeYesKeys.insert(edgeYesItr->first);
+	}
+	set<int>::iterator keys_itr;
+	for(keys_itr = edgeYesKeys.begin(); keys_itr != edgeYesKeys.end(); keys_itr++)
+	{
+		int u = *keys_itr;
+		visited_itr = visitedNode.find(u);
+		if(visited_itr == visitedNode.end())
+		{
+			DFS(u,u);
+			repComp.insert(u);
+		}
+	}
+	set<int> deleteClust;
+	set<int>::iterator del_itr;
+	map<int, set<int> >::iterator clus_itr;
+	set_difference(edgeYesKeys.begin(),edgeYesKeys.end(),repComp.begin(),repComp.end(),std::inserter(deleteClust,deleteClust.begin()));
+	for(del_itr = deleteClust.begin(); del_itr != deleteClust.end(); del_itr++)
+	{
+		int u = *del_itr;
+		clus_itr = clusters.find(u);
+		if(clus_itr != clusters.end())
+		{
+			clusters.erase(clus_itr);
+		}
+	}
+	edgeYes.clear();
+	set<int>::iterator rep_itr;
+	set<int>::iterator no_itr;
+	set<int>::iterator weak_itr;
+	map<int,set<int> >::iterator edgeNoItr;
+	map<int,set<int> >::iterator edgeWeakItr;
+
+	for(visited_itr = visitedNode.begin(); visited_itr != visitedNode.end(); visited_itr++)
+	{
+		int u = visited_itr->first;
+		int rep = visited_itr->second;
+		for(rep_itr = clusters[rep].begin(); rep_itr != clusters[rep].end(); rep_itr++)
+		{
+			int point = *rep_itr;
+			if(point == u)
+			{
+				continue;
+			}
+			edgeNoItr = edgeNo.find(u);
+			if(edgeNoItr != edgeNo.end())
+			{
+				no_itr = find(edgeNo[u].begin(),edgeNo[u].end(),point);
+				if(no_itr != edgeNo[u].end())
+				{
+					edgeNo[u].erase(no_itr);
+				}
+			}
+			edgeWeakItr = edgeWeak.find(u);
+			if(edgeWeakItr != edgeWeak.end())
+			{
+				weak_itr = find(edgeWeak[u].begin(),edgeWeak[u].end(),point);
+				if(weak_itr != edgeWeak[u].end())
+				{
+					edgeWeak[u].erase(weak_itr);
+				}
+			}
+		}
+		edgeNoItr = edgeNo.find(u);
+		if(edgeNoItr != edgeNo.end())
+		{
+			if(edgeNo[u].size() == 0)
+			{
+				edgeNo.erase(edgeNoItr);
+			}
+		}
+		edgeWeakItr = edgeWeak.find(u);
+		if(edgeWeakItr != edgeWeak.end())
+		{
+			if(edgeWeak[u].size() == 0)
+			{
+				edgeWeak.erase(edgeWeakItr);
+			}
+		}
+	}
+}
+
+void DFS(int u, int rep)
+{
+	visitedNode.insert(pair<int, int>(u,rep));
+	set<int>::iterator clus_itr;
+	for(clus_itr = clusters[u].begin(); clus_itr != clusters[u].end(); clus_itr++)
+	{
+		int v = *clus_itr;
+		clusters[rep].insert(v);
+	}
+	set<int>::iterator yes_itr;
+	map<int,int>::iterator visited_itr;
+	for(yes_itr = edgeYes[u].begin(); yes_itr != edgeYes[u].end(); yes_itr++)
+	{
+		int v = *yes_itr;
+		visited_itr = visitedNode.find(v);
+		if(visited_itr == visitedNode.end())
+		{
+			DFS(v,rep);
+		}
+	}
+}
+
+
 void anyDBC(int num_records, int dimension)
 {	//STARTING STEP:1
 	vector<int> untouchedList;
-	vector<int>::iterator itr;
+	vector<int>::iterator itr; 				//iterator for untouchedList
+	map<int,string>::iterator itr_core; 	//iterator for coreList
+	map<int,set<int> >::iterator itr_clus1;	//iterator for clusters
+	map<int,set<int> >::iterator itr_clus2;	//iterator for clusters
+	set<int>::iterator itr_done;			//iterator for donePoints
+
 	int i;
 	for(i = 0; i < num_records; i++)
 	{
@@ -289,12 +464,123 @@ void anyDBC(int num_records, int dimension)
 				untouchedList.erase(itr);
 				set<int> neighbours_of_point = performRangeQuery(point,num_records,dimension);
 				vector<int> touch = assignStateNei(point,neighbours_of_point);
-
+				itr_core = coreList.find(point);
+				if(itr_core != coreList.end())
+				{
+					createPCIR(point);
+				}
+				int j;
+				for(j = 0; j < touch.size(); j++)
+				{
+					int p = touch[i];
+					itr = find(untouchedList.begin(),untouchedList.end(),p);
+					if(itr != untouchedList.end())
+					{
+						untouchedList.erase(itr);
+					}
+				}
 			}
 		}
-
 	}
-
+	/*######################################################################*/
+	cout << "Initial count of range queries: "<< rangeQueryPerformed.size() << "\n";
+	set<int> donePoints;
+	int point;
+	map<int,set<int> >::iterator edgeNoItr;
+	map<int,set<int> >::iterator edgeYesItr;
+	map<int,set<int> >::iterator edgeWeakItr;
+	map<int,set<int> >::iterator edgeUnknownItr;
+	for(itr_clus1 = clusters.begin(); itr_clus1 != clusters.end(); itr_clus1++)
+	{
+		point = itr_clus1->first;
+		donePoints.insert(point);
+		int neiPoint;
+		for(itr_clus2 = clusters.begin(); itr_clus2 != clusters.end(); itr_clus2++)
+		{
+			neiPoint = itr_clus2->first;
+			itr_done = find(donePoints.begin(),donePoints.end(),neiPoint);
+			if(itr_done != donePoints.end())
+			{
+				continue;
+			}
+			int stat = ddcBetPCIR(point,neiPoint,dimension);
+			if(stat == 1)
+			{
+				edgeNoItr = edgeNo.find(point);
+				if(edgeNoItr == edgeNo.end())
+				{
+					set<int> s;
+					edgeNo.insert(pair<int,set<int> >(point,s));
+				}
+				edgeNo[point].insert(neiPoint);
+				edgeNoItr = edgeNo.find(neiPoint);
+				if(edgeNoItr == edgeNo.end())
+				{
+					set<int> s;
+					edgeNo.insert(pair<int,set<int> >(neiPoint,s));
+				}
+				edgeNo[neiPoint].insert(point); 
+			}
+			else if(stat == 0)
+			{
+				edgeYesItr = edgeYes.find(point);
+				if(edgeYesItr == edgeYes.end())
+				{
+					set<int> s;
+					edgeYes.insert(pair<int,set<int> >(point,s));
+				}
+				edgeYes[point].insert(neiPoint);
+				edgeYesItr = edgeYes.find(neiPoint);
+				if(edgeYesItr == edgeYes.end())
+				{
+					set<int> s;
+					edgeYes.insert(pair<int,set<int> >(neiPoint,s));
+				}
+				edgeYes[neiPoint].insert(point); 
+			}
+			else if(stat == 2)
+			{
+				edgeWeakItr = edgeWeak.find(point);
+				if(edgeWeakItr == edgeWeak.end())
+				{
+					set<int> s;
+					edgeWeak.insert(pair<int,set<int> >(point,s));
+				}
+				edgeWeak[point].insert(neiPoint);
+				edgeWeakItr = edgeWeak.find(neiPoint);
+				if(edgeWeakItr == edgeWeak.end())
+				{
+					set<int> s;
+					edgeWeak.insert(pair<int,set<int> >(neiPoint,s));
+				}
+				edgeWeak[neiPoint].insert(point); 
+			}
+			else if(stat == 3)
+			{
+				edgeUnknownItr = edgeUnknown.find(point);
+				if(edgeUnknownItr == edgeUnknown.end())
+				{
+					set<int> s;
+					edgeUnknown.insert(pair<int,set<int> >(point,s));
+				}
+				edgeUnknown[point].insert(neiPoint);
+				edgeUnknownItr = edgeUnknown.find(neiPoint);
+				if(edgeUnknownItr == edgeUnknown.end())
+				{
+					set<int> s;
+					edgeUnknown.insert(pair<int,set<int> >(neiPoint,s));
+				}
+				edgeUnknown[neiPoint].insert(point); 				
+			}
+		}
+	}
+	/*######################################################################*/
+	if(edgeYes.size() > 0)
+	{
+		connComp();
+		//mergeAssignNewEdge();
+	}
+	int iteration = 0;
 }
 
 int main(int argc, char* argv[])
